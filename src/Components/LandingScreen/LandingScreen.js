77c0c675b726f6ce68b90d35705b80b8204ed79d
Darek Radke <dradmclad@gmail.com>
2022-05-17 16:27:03 -0500

    class names and keyframe animations for first section

--- src/Components/LandingScreen/LandingScreen.js
+++ src/Components/LandingScreen/LandingScreen.js
@@ -6,16 +6,16 @@ const landingScreen = props => {
     return (
         <div className="landingScreen" ref={props.landingRef}>
             <div className="landing-screen-content">
-                <p>D</p>
-                <p>A</p>
-                <p>R</p>
-                <p>E</p>
-                <p>K</p>
-                <p>R</p>
-                <p>A</p>
-                <p>D</p>
-                <p>K</p>
-                <p>E</p>
+                <p className="firstL">D</p>
+                <p className="secondL">A</p>
+                <p className="thirdL">R</p>
+                <p className="fourthL">E</p>
+                <p className="fithL">K</p>
+                <p className="sixthL">R</p>
+                <p className="seventhL">A</p>
+                <p className="eighthL">D</p>
+                <p className="ninthL">K</p>
+                <p className="tenthL">E</p>
             </div>
         </div>
     );
