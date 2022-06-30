03e824620b737ba26e04164fbc01f0c079b756e8
Darek Radke <dradmclad@gmail.com>
2022-06-30 13:44:39 -0500

    react-image-gallery

--- src/Components/Carousel/Carousel.js
+++ src/Components/Carousel/Carousel.js
@@ -1,26 +1,26 @@
 import React from "react";
-import { Carousel } from 'react-responsive-carousel';
+import './Carousel.css';
+import ImageGallery from 'react-image-gallery';
 import img1 from '../../static/images/img1.png';
 import img2 from '../../static/images/img2.png';
 import img3 from '../../static/images/img3.png';
 
+const images = [
+  {
+    original: img1,
+  },
+  {
+    original: img2,
+  },
+  {
+    original: img3,
+  },
+];
+
 const Carousel = () => {
     return (
-        <div className="carouselContainer">
-          <Carousel>
-                <div>
-                    <img src={img1} />
-                    <p className="legend">Legend 1</p>
-                </div>
-                <div>
-                    <img src={img2} />
-                    <p className="legend">Legend 2</p>
-                </div>
-                <div>
-                    <img src={img3} />
-                    <p className="legend">Legend 3</p>
-                </div>
-          </Carousel>
+        <div className="Carousel">
+          <ImageGallery items={images} />
         </div>
       );
 }
--- src/Components/SecondScreen/SecondScreen.js
+++ src/Components/SecondScreen/SecondScreen.js
@@ -1,12 +1,13 @@
 import React from "react";
 import './SecondScreen.css'
+import Carousel from '../Carousel/Carousel.js'
 
 
 const secondScreen = props => {
 
     return (
         <div className="secondScreen" ref={props.secondRef}>
-            <p className="secondScreenContent">Second Screen</p>
+            <Carousel />
         </div>
     );
 };
