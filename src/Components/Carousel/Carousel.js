3140f5ba55710899d3bab609aa81c6de79ff5b73
Darek Radke <dradmclad@gmail.com>
2022-06-30 12:36:20 -0500

    added placeholder images

--- src/Components/Carousel/Carousel.js
+++ src/Components/Carousel/Carousel.js
@@ -1,20 +1,23 @@
 import React from "react";
 import { Carousel } from 'react-responsive-carousel';
+import img1 from '../../static/images/img1.png';
+import img2 from '../../static/images/img2.png';
+import img3 from '../../static/images/img3.png';
 
 const Carousel = () => {
     return (
         <div className="carouselContainer">
           <Carousel>
                 <div>
-                    <img src="" />
+                    <img src={img1} />
                     <p className="legend">Legend 1</p>
                 </div>
                 <div>
-                    <img src="" />
+                    <img src={img2} />
                     <p className="legend">Legend 2</p>
                 </div>
                 <div>
-                    <img src="" />
+                    <img src={img3} />
                     <p className="legend">Legend 3</p>
                 </div>
           </Carousel>
