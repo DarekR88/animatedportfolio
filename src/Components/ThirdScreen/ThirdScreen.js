1d50e413d22e89f882fa355e999242588eccd006
Darek Radke <dradmclad@gmail.com>
2022-07-03 22:58:54 -0500

    emailjs set up

--- src/Components/ThirdScreen/ThirdScreen.js
+++ src/Components/ThirdScreen/ThirdScreen.js
@@ -3,9 +3,33 @@ import './ThirdScreen.css'
 
 
 const ThirdScreen = props => {
+
+    const form = useRef();
+
+    const sendEmail = (e) => {
+      e.preventDefault();
+  
+      emailjs.sendForm('service_qfgklbh', 'YOUR_TEMPLATE_ID', form.current, 'DfRYHaxL9gIFFEmtj')
+        .then((result) => {
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text);
+        });
+
+    }
+
+
     return (
         <div className="thirdScreen" ref={props.thirdRef}>
-            <p className="third-screen-content">Third Screen</p>
+            <form ref={form} onSubmit={sendEmail}>
+                <label>Name</label>
+                <input type="text" name="user_name" />
+                <label>Email</label>
+                <input type="email" name="user_email" />
+                <label>Message</label>
+                <textarea name="message" />
+                <input type="submit" value="Send" />
+            </form>
         </div>
     );
 };
