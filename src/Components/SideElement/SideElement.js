c1d4a309f8c3e369bab46f3aa5f06e29440ca173
Darek Radke <dradmclad@gmail.com>
2022-07-06 17:45:09 -0500

    sideElement files created

---
+++ src/Components/SideElement/SideElement.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './SideElement.css'
+
+
+const SideElement = props => {
+
+
+
+    return (
+        <div className="sideElemntWrapper">
+
+        </div>
+    );
+};
+
+export default SideElement;
