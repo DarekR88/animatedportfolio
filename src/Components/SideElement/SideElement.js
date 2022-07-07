f77abab4a4fcb4ea957f347bb8d3ec37bbd2075f
Darek Radke <dradmclad@gmail.com>
2022-07-07 10:58:40 -0500

    svg icons

--- src/Components/SideElement/SideElement.js
+++ src/Components/SideElement/SideElement.js
@@ -1,5 +1,7 @@
 import React from "react";
 import './SideElement.css'
+import githubIcon from "../../static/icons/github_icon.svg";
+import linkedinIcon from "../../static/icons/linkedin_icon.svg";
 
 
 const SideElement = props => {
@@ -8,7 +10,11 @@ const SideElement = props => {
 
     return (
         <div className="sideElemntWrapper">
-
+            <ul>
+                <li><a href="" rel="noreferrer" className="side-a">{githubIcon}</a></li>
+                <li><a href="" rel="noreferrer" className="side-a">{linkedinIcon}</a></li>
+                {/* <li><a href="" rel="noreferrer" className="side-a">Contact</a></li> */}
+            </ul>
         </div>
     );
 };
